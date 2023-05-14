aa59ea7b4a1dd0bf6c7e66163236e2ed2ebbf9b0
KTELLAWI <koutaiba@msn.com>
2023-05-14 17:48:46 +0300

    file35

--- src/app/api/index/route.ts
+++ src/app/api/index/route.ts
@@ -20,7 +20,7 @@ import {
 
 
 export const vectorStore = async(querystring:string)=>{
-    const loader = new TextLoader("/alhaj.txt");
+    const loader = new TextLoader("https://openaichatbot-nt5p.vercel.app/alhaj.txt");
   const docs = await loader.load();
   const splitter = new RecursiveCharacterTextSplitter({
     chunkSize: 1000,
