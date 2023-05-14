5648a02200390eda21e8fe1a5ac87348eb38f491
KTELLAWI <koutaiba@msn.com>
2023-05-14 14:56:48 +0300

    file32

--- src/app/api/index/route.ts
+++ src/app/api/index/route.ts
@@ -20,7 +20,7 @@ import {
 
 
 export const vectorStore = async(querystring:string)=>{
-    const loader = new TextLoader("/alhaj.txt");
+    const loader = new TextLoader(process.env.FILE_PATH);
   const docs = await loader.load();
   const splitter = new RecursiveCharacterTextSplitter({
     chunkSize: 1000,
