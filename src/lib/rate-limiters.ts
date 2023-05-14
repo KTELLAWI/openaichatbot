4bf14dcb9fda8f167afa2f998cbaee99eba96615
KTELLAWI <koutaiba@msn.com>
2023-05-14 23:01:01 +0300

    ratelimit

--- src/lib/rate-limiters.ts
+++ src/lib/rate-limiters.ts
@@ -4,7 +4,7 @@ import { Ratelimit } from '@upstash/ratelimit'
 // Create a new ratelimiter, that allows 10 requests per 10 seconds
 export const rateLimiter = new Ratelimit({
   redis,
-  limiter: Ratelimit.slidingWindow(12, '70 m'),
+  limiter: Ratelimit.slidingWindow(3, '70 m'),
   /**
    * Optional prefix for the keys used in redis. This is useful if you want to share a redis
    * instance with other applications and want to avoid key collisions. The default prefix is
